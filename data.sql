2223c289ec64c8107a521f0af7bd0528cf3426d0
Chris Wolf <chriswolfdesign@gmail.com>
2021-01-12 17:14:40 -0500

    Updated tables to be joinable with pokemon.id

--- data.sql
+++ data.sql
@@ -49,10 +49,10 @@ DROP TABLE IF EXISTS `pokemon_abilities`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pokemon_abilities` (
   `Id` int NOT NULL,
-  `pokedex_number` int NOT NULL,
   `ability1` varchar(20) NOT NULL,
   `ability2` varchar(20) DEFAULT NULL,
   `hidden_ability` varchar(20) DEFAULT NULL,
+  `pokemon_id` int NOT NULL,
   UNIQUE KEY `Id` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -63,7 +63,7 @@ CREATE TABLE `pokemon_abilities` (
 
 LOCK TABLES `pokemon_abilities` WRITE;
 /*!40000 ALTER TABLE `pokemon_abilities` DISABLE KEYS */;
-INSERT INTO `pokemon_abilities` VALUES (1,1,'Overgrow',NULL,'Chloropyll'),(2,2,'Overgrow',NULL,'Chloropyll'),(3,3,'Overgrow',NULL,'Chloropyll'),(4,4,'Blaze',NULL,'Solar Power'),(5,5,'Blaze',NULL,'Solar Power'),(6,6,'Blaze',NULL,'Solar Power'),(7,7,'Torrent',NULL,'Rain Dish'),(8,8,'Torrent',NULL,'Rain Dish'),(9,9,'Torrent',NULL,'Rain Dish');
+INSERT INTO `pokemon_abilities` VALUES (1,'Overgrow',NULL,'Chlorophyll',1),(2,'Overgrow',NULL,'Chlorophyll',2),(3,'Overgrow',NULL,'Chlorophyll',3),(4,'Blaze',NULL,'Solar Power',4),(5,'Blaze',NULL,'Solar Power',5),(6,'Blaze',NULL,'Solar Power',6),(7,'Torrent',NULL,'Rain Dish',7),(8,'Torrent',NULL,'Rain Dish',8),(9,'Torrent',NULL,'Rain Dish',9);
 /*!40000 ALTER TABLE `pokemon_abilities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,9 +76,9 @@ DROP TABLE IF EXISTS `pokemon_egg_groups`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pokemon_egg_groups` (
   `Id` int NOT NULL,
-  `pokedex_number` int NOT NULL,
   `egg_group1` varchar(20) DEFAULT NULL,
   `egg_group2` varchar(20) DEFAULT NULL,
+  `pokemon_id` int NOT NULL,
   UNIQUE KEY `Id` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -89,7 +89,7 @@ CREATE TABLE `pokemon_egg_groups` (
 
 LOCK TABLES `pokemon_egg_groups` WRITE;
 /*!40000 ALTER TABLE `pokemon_egg_groups` DISABLE KEYS */;
-INSERT INTO `pokemon_egg_groups` VALUES (1,1,'monster','grass'),(2,2,'monster','grass'),(3,3,'monster','grass'),(4,4,'monster','dragon'),(5,5,'monster','dragon'),(6,6,'monster','dragon'),(7,7,'monster','water 1'),(8,8,'monster','water 1'),(9,9,'monster','water 1');
+INSERT INTO `pokemon_egg_groups` VALUES (1,'monster','grass',1),(2,'monster','grass',2),(3,'monster','grass',3),(4,'monster','dragon',4),(5,'monster','dragon',5),(6,'monster','dragon',6),(7,'monster','water 1',7),(8,'monster','water 1',8),(9,'monster','water 1',9);
 /*!40000 ALTER TABLE `pokemon_egg_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,9 +102,9 @@ DROP TABLE IF EXISTS `pokemon_types`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pokemon_types` (
   `Id` int NOT NULL,
-  `pokedex_number` int NOT NULL,
   `type1` varchar(20) NOT NULL,
   `type2` varchar(20) DEFAULT NULL,
+  `pokemon_id` int NOT NULL,
   UNIQUE KEY `Id` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -115,7 +115,7 @@ CREATE TABLE `pokemon_types` (
 
 LOCK TABLES `pokemon_types` WRITE;
 /*!40000 ALTER TABLE `pokemon_types` DISABLE KEYS */;
-INSERT INTO `pokemon_types` VALUES (1,1,'grass','poison'),(2,2,'grass','poison'),(3,3,'grass','poison'),(4,4,'fire',NULL),(5,5,'fire',NULL),(6,6,'fire','flying'),(7,7,'water',NULL),(8,8,'water',NULL),(9,9,'water',NULL);
+INSERT INTO `pokemon_types` VALUES (1,'grass','poison',1),(2,'grass','poison',2),(3,'grass','poison',3),(4,'fire',NULL,4),(5,'fire',NULL,5),(6,'fire','flying',6),(7,'water',NULL,7),(8,'water',NULL,8),(9,'water',NULL,9);
 /*!40000 ALTER TABLE `pokemon_types` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -128,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-05 18:43:02
+-- Dump completed on 2021-01-12 17:14:04
